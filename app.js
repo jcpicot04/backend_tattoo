c8f0a9fc5e08cb741a3f7397c4bec04e0ab76272
jcpicot04 <jcpicot1718@gmail.com>
2022-06-08 02:21:57 +0200

    fixing cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 
 const corOptions = {
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000',"https://tatuate.herokuapp.com/";
 };
 
 const swaggerOptions = {
