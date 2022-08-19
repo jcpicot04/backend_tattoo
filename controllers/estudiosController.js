cf1707b6000227c5636a7cd24a99d691ce3ba368
jcpicot04 <jcpicot1718@gmail.com>
2022-08-19 13:21:41 +0200

    new logic for estudios

--- controllers/estudiosController.js
+++ controllers/estudiosController.js
@@ -3,15 +3,45 @@ const db = require("../models");
 const Estudios = db.estudios
 
 const addEstudio = async (req, res) => {
+if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send("No se ha subido imagen del tatuador.");
+}
     let info = {
         nombre: req.body.nombre,
         latitud: req.body.latitud,
         longitud: req.body.longitud,
-        imagen: req.body.imagen
+        imagen: ""
     }
 
+try {
     const estudio = await Estudios.create(info);
-    res.status(200).send(estudio);
+
+    if (estudio) {
+        let file = req.files.imagen;
+        var fs = require("fs");
+        var shell = require("shelljs");
+        const pathFile =
+          require("path").resolve(__dirname, "..") +
+          `/public/estudios/${estudio.id}`;
+        if (!fs.existsSync(pathFile)) {
+          shell.mkdir("-p", pathFile);
+        }
+        file.mv(pathFile + "/logo.jpg", function (err) {
+          if (err) return res.status(500).send(err);
+        });
+  
+        await estudio.update(
+          { imagen: `./public/estudios/${estudio.id}/logo.jpg` },
+          { where: { id: estudio.id } }
+        );
+  
+        res.status(200).send(estudio);
+      } else {
+        res.status(500).json({ error: "Ha ocurrido un error" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Ha ocurrido un error" });
+    }
 }
 
 const getAllEstudios = async (req, res) => {
